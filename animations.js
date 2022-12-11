b8e0256dfe0cfc0b7a545bcadc242289df01d2aa
Sam Sweeney <samsweeneyy@gmail.com>
2022-12-11 14:32:28 +1100

    Changed animations and styles

--- animations.js
+++ animations.js
@@ -10,8 +10,10 @@ let transitionDelayTime = 0;
 export const animateCards = (cards) => {
   cards.forEach((card) => {
     card.style.transitionDelay = `${transitionDelayTime}ms`
+
     observer.observe(card);
     transitionDelayTime += 200;
 
   });
+  transitionDelayTime = 0;
 };
